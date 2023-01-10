b50c47f3270ac54faddcbbcebc9aba96136c9cdc
Dmitrii <aleksdima@mail.ru>
2023-01-10 12:55:05 +0300

    Added search method

--- Work/Program.cs
+++ Work/Program.cs
@@ -2,3 +2,26 @@
 Console.Write("Введите элементы строки через пробел:");
 string[] array = Console.ReadLine().Split(" ");
 Console.WriteLine($"[{string.Join(", ", array)}]");
+void Search(string[] array)
+{
+    int l = 0, j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            l++;
+    }
+
+    string[] resultArray = new string[l];
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i].Length <= 3)
+        {
+            resultArray[j] = array[i];
+            j++;
+        }
+    }
+
+    Console.WriteLine($"[{string.Join(", ", resultArray)}]");
+}
+Search(array);
