23d342b42e1f1fcba30de3b455025c3b5eebac41
Dmitrii <aleksdima@mail.ru>
2023-01-10 15:03:40 +0300

    Added check for empty strings

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,27 +1,35 @@
 ﻿Console.Clear();
 Console.Write("Введите элементы строки через пробел:");
 string[] array = Console.ReadLine().Split(" ");
-Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
 void Search(string[] array)
 {
     int l = 0, j = 0;
     for (int i = 0; i < array.Length; i++)
     {
+
         if (array[i].Length <= 3)
+        {
+            if (String.IsNullOrEmpty(array[i]))
+                continue;
             l++;
+        }
     }
 
     string[] resultArray = new string[l];
     for (int i = 0; i < array.Length; i++)
     {
 
+
         if (array[i].Length <= 3)
         {
+            if (String.IsNullOrEmpty(array[i]))
+                continue;
             resultArray[j] = array[i];
             j++;
         }
-    }
 
+    }
     Console.WriteLine($"[{string.Join(", ", resultArray)}]");
 }
 Search(array);
